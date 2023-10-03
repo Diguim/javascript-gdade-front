ceb4f454e8055b3099be82b1e8ba99cb0222a7ec
Diogo Otoni <diogootoni@hotmail.com>
2023-10-03 11:23:29 -0300

    Basico de cadastrar e atualizar funcionando

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,23 +2,20 @@ const addButton = document.getElementById("add-button");
 const searchButton = document.getElementById("search-button");
 const attButton = document.getElementById("att-button");
 
-function register(values){
-    
+function register(values) {
     //recebo os valores do formulario e envio para o back-end
-    
     fetch("http://localhost:8080/api/jobs", {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(values),
-    }).catch(error => {
+    }).catch((error) => {
         console.log(error);
-    })
-    
+    });
 }
 
-addButton.addEventListener('click', (e) => {
+addButton.addEventListener("click", (e) => {
     e.preventDefault();
     const nameJob = document.querySelector("#name-job").value;
     const descJob = document.querySelector("#desc-job").value;
@@ -26,25 +23,57 @@ addButton.addEventListener('click', (e) => {
     const linkJob = document.querySelector("#link-job").value;
     const empAnunJob = document.querySelector("#emp-anun-job").value;
     const linkAnunJob = document.querySelector("#link-anun-job").value;
-    
+
     const valores = {
         jobName: nameJob,
         descricao: descJob,
         empresa: empresaJob,
         link: linkJob,
         empAnun: empAnunJob,
-        linkAnun: linkAnunJob
-    }
+        linkAnun: linkAnunJob,
+    };
     register(valores);
-})
+});
 
-searchButton.addEventListener('click', (e) => {
+function putInHtml(dados) {
+    const contentShowSection = document.getElementById("content-show-section");
+    console.log(dados);
+    let newHtml = "";
+    for(let i = 1; i < dados.length; i++){
+        newHtml += `
+        <div class="content-cards-jobs">
+            <h3>${dados[i].jobName}</h3>
+            <p>${dados[i].descricao}</p>
+            <p>${dados[i].empresa}</p>
+            <p>${dados[i].link}</p>
+            <p>${dados[i].empAnunJob}</p>
+            <p>${dados[i].linkAnunJob}</p>
+            <p>dia</p>
+            <p>hora</p>
+        </div>`;
+    }
+
+    contentShowSection.innerHTML = newHtml;
+}
+
+function attList() {
+    fetch("http://localhost:8080/api/jobs")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            putInHtml(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+attButton.addEventListener("click", () => {
+    attList();
+});
+
+searchButton.addEventListener("click", (e) => {
     e.preventDefault();
     //faz a busca de acordo com o escolhido no option[value]
     console.log("clicou no searchButton");
-})
-
-attButton.addEventListener('click', () => {
-    //atualiza todos os cards
-    console.log("clicou no attButton");
-})
+});
